797fe132d8b087d2d5a41e40f85f77d5f4a46690
Sarah Lock <Sarah.lock@york.ac.uk>
2026-01-23 19:18:53 +0800

    Jan26-update4

---
+++ R/TimeTraits.R
@@ -0,0 +1,12 @@
+#' TimeTraits
+#'
+#' Contains functions for the analysis of biological time-series data.
+#'     Use four spaces when indenting paragraphs within the Description.
+#'
+#' @aliases <TimeTraits>-package
+#' @name TimeTraits
+#'
+#' @author Sarah Lock \email{sarah.lock@york.ac.uk}
+#'
+NULL
+
